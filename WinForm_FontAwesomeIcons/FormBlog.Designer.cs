a541eae6de832b3746b1d40d0ead4e5e08973b04
upwon <1244753906@qq.com>
2020-05-02 22:18:00 +0800

    add readme

---
+++ WinForm_FontAwesomeIcons/FormBlog.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WinForm_FontAwesomeIcons
+{
+    partial class FormBlog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(841, 548);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser2.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(841, 548);
+            this.webBrowser2.TabIndex = 1;
+            // 
+            // FormBlog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 548);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "FormBlog";
+            this.Text = "FormBlog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,79 @@
+# image_bed
+
+##  [中文看这里](##中文)
+
+### Description
+
+Refer to this [video](https://youtu.be/K2lgEPdhGcg) to complete the simple Program, just an interface of WinForm, flat  design, without any other functions,  can be used as a template to develop some simple Forms.  The icon uses [FontAwesome.Sharp](  https://github.com/awesome-inc/FontAwesome.Sharp) . For more information, please visit my blog
+
+https://upwon.github.io/
+
+
+
+### Software Architecture
+
+It is just a WinForm interface.
+
+
+
+### Installation tutorial
+
+1. Open the solution.
+
+2. Add the nuget installation package in the package manager: FontAwesome.Sharp.
+
+3. Generate the solution and run it.
+
+   
+
+### Instructions for use
+
+1. Add your own functions on this basis.
+
+2. To change the button icon, just change IconChar.
+
+3. You can check the name of the icon in [fontawesome] (https://fontawesome.com/icons?d=gallery&m=free) and then change IconChar to make it faster.
+
+   
+
+### Features
+
+1. Click the button to change the icon color
+2. The form adaptively changes the size, the form supports dragging, maximize and minimize operation
+3. The child form is embedded in the panel in the main form
+
+
+
+## 中文
+
+### 介绍 [English](##Description)  
+
+参考视频 [https://youtu.be/K2lgEPdhGcg](https://youtu.be/K2lgEPdhGcg) 完成的一个小程序，仅仅是一个界面，扁平化设计，没有任何其它功能，可作为开发一些简单窗体的模板。图标使用的是 [FontAwesome.Sharp](  https://github.com/awesome-inc/FontAwesome.Sharp) 。更多资讯请访问我的博客查阅
+
+https://upwon.github.io/
+
+### 软件架构
+
+仅仅是一个 WinForm 窗体界面。
+
+### 安装教程
+
+1. 打开解决方案。
+2. 在包管理器中添加 nuget 安装包 ： FontAwesome.Sharp 。
+3. 生成解决方案，运行即可。
+
+### 使用说明
+
+1. 在此基础上加入自己的功能。
+2. 更改按钮图标请更改 IconChar 即可。
+3. 可以在 [fontawesome](https://fontawesome.com/icons?d=gallery&m=free)中查看图标的名称，然后更改 IconChar ，这样更加快捷。
+
+### 特性
+
+1. 点击按钮可改变图标颜色
+
+2. 窗体自适应更改大小，窗体支持拖动，最大化最小化操作
+
+3. 子窗体嵌入主窗体中的pannel
+
+   
