23debb1c612e8e7b9f007964b584d565f4c2654a
upwon <1244753906@qq.com>
2020-05-03 12:46:49 +0800

    add browser

--- WinForm_FontAwesomeIcons/FormBlog.Designer.cs
+++ WinForm_FontAwesomeIcons/FormBlog.Designer.cs
@@ -28,44 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.forwardButton = new FontAwesome.Sharp.IconButton();
+            this.backButton = new FontAwesome.Sharp.IconButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.iconButtonGo = new FontAwesome.Sharp.IconButton();
             this.SuspendLayout();
             // 
-            // webBrowser1
+            // outputLabel
             // 
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(841, 548);
-            this.webBrowser1.TabIndex = 0;
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(866, 5);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(55, 15);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "label1";
             // 
-            // webBrowser2
+            // forwardButton
             // 
-            this.webBrowser2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser2.Location = new System.Drawing.Point(0, 0);
-            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser2.Name = "webBrowser2";
-            this.webBrowser2.Size = new System.Drawing.Size(841, 548);
-            this.webBrowser2.TabIndex = 1;
+            this.forwardButton.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.forwardButton.ForeColor = System.Drawing.Color.White;
+            this.forwardButton.IconChar = FontAwesome.Sharp.IconChar.ArrowAltCircleRight;
+            this.forwardButton.IconColor = System.Drawing.Color.White;
+            this.forwardButton.IconSize = 32;
+            this.forwardButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.forwardButton.Location = new System.Drawing.Point(119, -10);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Rotation = 0D;
+            this.forwardButton.Size = new System.Drawing.Size(109, 50);
+            this.forwardButton.TabIndex = 2;
+            this.forwardButton.Text = "Forward";
+            this.forwardButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.forwardButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.forwardButton.UseVisualStyleBackColor = true;
+            // 
+            // backButton
+            // 
+            this.backButton.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.backButton.ForeColor = System.Drawing.Color.White;
+            this.backButton.IconChar = FontAwesome.Sharp.IconChar.ArrowAltCircleLeft;
+            this.backButton.IconColor = System.Drawing.Color.White;
+            this.backButton.IconSize = 32;
+            this.backButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.backButton.Location = new System.Drawing.Point(4, -10);
+            this.backButton.Name = "backButton";
+            this.backButton.Rotation = 0D;
+            this.backButton.Size = new System.Drawing.Size(109, 50);
+            this.backButton.TabIndex = 3;
+            this.backButton.Text = "Back";
+            this.backButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.backButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(478, 25);
+            this.textBox1.TabIndex = 4;
+            // 
+            // iconButtonGo
+            // 
+            this.iconButtonGo.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.iconButtonGo.ForeColor = System.Drawing.Color.White;
+            this.iconButtonGo.IconChar = FontAwesome.Sharp.IconChar.Google;
+            this.iconButtonGo.IconColor = System.Drawing.Color.White;
+            this.iconButtonGo.IconSize = 32;
+            this.iconButtonGo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButtonGo.Location = new System.Drawing.Point(718, 5);
+            this.iconButtonGo.Name = "iconButtonGo";
+            this.iconButtonGo.Rotation = 0D;
+            this.iconButtonGo.Size = new System.Drawing.Size(89, 57);
+            this.iconButtonGo.TabIndex = 5;
+            this.iconButtonGo.Text = "Forward";
+            this.iconButtonGo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.iconButtonGo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButtonGo.UseVisualStyleBackColor = true;
             // 
             // FormBlog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(841, 548);
-            this.Controls.Add(this.webBrowser2);
-            this.Controls.Add(this.webBrowser1);
+            this.ClientSize = new System.Drawing.Size(933, 548);
+            this.Controls.Add(this.iconButtonGo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.forwardButton);
+            this.Controls.Add(this.outputLabel);
             this.Name = "FormBlog";
             this.Text = "FormBlog";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.WebBrowser webBrowser1;
-        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Label outputLabel;
+        private FontAwesome.Sharp.IconButton forwardButton;
+        private FontAwesome.Sharp.IconButton backButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private FontAwesome.Sharp.IconButton iconButtonGo;
     }
 }
--- WinForm_FontAwesomeIcons/FormBlog.cs
+++ WinForm_FontAwesomeIcons/FormBlog.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using  CefSharp.WinForms;
+
 
 namespace WinForm_FontAwesomeIcons
 {
@@ -15,6 +18,106 @@ namespace WinForm_FontAwesomeIcons
         public FormBlog()
         {
             InitializeComponent();
+            InitBrowser();
         }
+        public ChromiumWebBrowser browser;
+        public void InitBrowser()
+        {
+            
+        //    Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("https://upwon.github.io/");
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+            browser.IsBrowserInitializedChanged += OnIsBrowserInitializedChanged;
+         //   browser.LoadingStateChanged += OnLoadingStateChanged;
+            browser.ConsoleMessage += OnBrowserConsoleMessage;
+         //   browser.StatusMessage += OnBrowserStatusMessage;
+            browser.TitleChanged += OnBrowserTitleChanged;
+         //  browser.AddressChanged += OnBrowserAddressChanged;
+
+            var version = string.Format("Chromium: {0}, CEF: {1}, CefSharp: {2}",
+                Cef.ChromiumVersion, Cef.CefVersion, Cef.CefSharpVersion);
+
+
+
+#if NETCOREAPP
+            // .NET Core
+            var environment = string.Format("Environment: {0}, Runtime: {1}",
+                System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant(),
+                System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+#else
+            // .NET Framework
+            var bitness = Environment.Is64BitProcess ? "x64" : "x86";
+            var environment = String.Format("Environment: {0}", bitness);
+#endif
+
+            DisplayOutput(string.Format("{0}, {1}", version, environment));
+        }
+
+        public void DisplayOutput(string output)
+        {
+            this.InvokeOnUiThreadIfRequired(() => outputLabel.Text = output);
+        }
+
+
+        private void OnIsBrowserInitializedChanged(object sender, EventArgs e)
+        {
+            var b = ((ChromiumWebBrowser)sender);
+
+            this.InvokeOnUiThreadIfRequired(() => b.Focus());
+        }
+
+        private void OnBrowserConsoleMessage(object sender, ConsoleMessageEventArgs args)
+        {
+            DisplayOutput(string.Format("Line: {0}, Source: {1}, Message: {2}", args.Line, args.Source, args.Message));
+        }
+
+    
+
+   
+        private void OnBrowserTitleChanged(object sender, TitleChangedEventArgs args)
+        {
+            this.InvokeOnUiThreadIfRequired(() => Text = args.Title);
+        }
+
+     
+
+    
+  
+     
+       
+        private void ExitMenuItemClick(object sender, EventArgs e)
+        {
+            browser.Dispose();
+            Cef.Shutdown();
+            Close();
+        }
+
+   
+        private void BackButtonClick(object sender, EventArgs e)
+        {
+            browser.Back();
+        }
+
+        private void ForwardButtonClick(object sender, EventArgs e)
+        {
+            browser.Forward();
+        }
+
+  
+
+        private void LoadUrl(string url)
+        {
+            if (Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+            {
+                browser.Load(url);
+            }
+        }
+
+        private void ShowDevToolsMenuItemClick(object sender, EventArgs e)
+        {
+            browser.ShowDevTools();
+        }
+
     }
 }
